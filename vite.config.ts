cd7e771e036058cc6470c8dccf0ac5ee4edf171e
Tinglin Huang <huangtinglin@outlook.com>
2026-01-02 16:52:46 -0500

    fix: update base path and clean locks

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cpsc483-583-website-24fall/",
+  base: "/cpsc483-583-website-26spring/",
   plugins: [
     vue({ 
       template: { transformAssetUrls }
